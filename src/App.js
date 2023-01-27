b23596c79e50f23fa18b39743b89e945675b1d2c
Mizanur <jameslev2001@gmail.com>
2023-01-27 17:35:15 +0530

    initial Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import dbRef, { userName } from './server/firebase';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {userName}
+    </div>
+  );
+}
+
+export default App;
