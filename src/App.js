290ff1d4edb8a699775230204e04961c5aa174cf
Mizanur <jameslev2001@gmail.com>
2023-01-28 13:34:33 +0530

    Add user into firebase

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-import dbRef, { userName } from './server/firebase';
+import dbRef, { userName, connectedRef } from './server/firebase';
 import './App.css';
+import { useEffect } from 'react';
 
 function App() {
+  const participantRef = dbRef.child('participants')
+  useEffect(() => {
+    connectedRef.on("value", (snap) => {
+      if (snap.val()) {
+        const defaultPreferences = {
+          audio: true,
+          video: false,
+          screen: false
+        }
+        const userRef = participantRef.push({
+          userName,
+          preference: defaultPreferences
+        })
+        userRef.onDisconnect().remove();
+      }
+    })
+  }, [])
   return (
     <div className="App">
       {userName}
